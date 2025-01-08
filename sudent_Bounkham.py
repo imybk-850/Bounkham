be1b6a34705cb2e004b05d60b652b232e6d8164d
imybk-850 <sbk796301@gmail.com>
2025-01-08 16:20:01 +0700

    Upload

---
+++ sudent_Bounkham.py
@@ -0,0 +1,9 @@
+from Student import Student
+
+class Bounkham(Student):
+    def name(self):
+        return "Sengsavangvong Bounkham"
+    def speak(self):
+        return "Toi ten la: Bounkham.\nGmail: sbk796307@gmail.com"
+    def address(self):
+        return "DH Quang Binh"
